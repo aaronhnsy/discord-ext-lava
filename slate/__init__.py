9be915cd1fbd1192ad0b1424c29440ea211b0bda
Aaron Hennessey <mynamebemrrandom@gmail.com>
2021-03-26 19:55:15 +0000

    reorder imports in __init__.py

--- slate/__init__.py
+++ slate/__init__.py
@@ -7,21 +7,18 @@ __version__ = '0.2.0'
 import logging
 from collections import namedtuple
 
+from .andesite.node import AndesiteNode
+from .bases.node import BaseNode
+from .bases.player import BasePlayer
 from .client import Client
 from .exceptions import *
+from .lavalink.node import LavalinkNode
 from .objects.events import TrackEndEvent, TrackExceptionEvent, TrackStartEvent, TrackStuckEvent, WebSocketClosedEvent
-from .objects.filters import Equalizer, Filter, Karaoke, Timescale, Tremolo, Vibrato, Rotation, LowPass, ChannelMix
+from .objects.filters import ChannelMix, Equalizer, Filter, Karaoke, LowPass, Rotation, Timescale, Tremolo, Vibrato
 from .objects.playlist import Playlist
 from .objects.stats import AndesiteStats, LavalinkStats, Metadata
 from .objects.track import Track
 from .utils import ExponentialBackoff, Queue
-
-from .bases.node import BaseNode
-from .bases.player import BasePlayer
-
-from .andesite.node import AndesiteNode
-from .lavalink.node import LavalinkNode
-
 
 version_info = namedtuple('VersionInfo', 'major minor micro releaselevel serial')(major=0, minor=2, micro=0, releaselevel='alpha', serial=0)
 logging.getLogger('slate')
