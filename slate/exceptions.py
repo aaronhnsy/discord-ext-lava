6ab57cf1680a41b652d2e8e5ce7530652f42f5fc
Aaron Hennessey <mynamebemrrandom@gmail.com>
2021-03-30 19:15:19 +0100

    remove unnecessary pass's

--- slate/exceptions.py
+++ slate/exceptions.py
@@ -9,14 +9,12 @@ class SlateException(Exception):
     """
     The base exception from which all Slate exceptions derive from.
     """
-    pass
 
 
 class NodeException(SlateException):
     """
     Base for exceptions raised for node related errors.
     """
-    pass
 
 
 class NodeCreationError(NodeException):
@@ -24,42 +22,36 @@ class NodeCreationError(NodeException):
     Raised when there was an error creating a node such as a duplicate identifier or the node class being passed to :py:meth:`Client.create_node` not being a subclass of
     :py:class:`BaseNode`.
     """
-    pass
 
 
 class NodeConnectionError(NodeException):
     """
     Raised when there was an error while connecting to a nodes external websocket. Could be something like invalid passwords, host address's, ports, etc.
     """
-    pass
 
 
 class NodeConnectionClosed(NodeException):
     """
     Raised when a nodes connection with it's external websocket was closed.
     """
-    pass
 
 
 class NodeNotFound(NodeException):
     """
     Raised when a node with the given identifier was not found.
     """
-    pass
 
 
 class NoNodesAvailable(NodeException):
     """
     Raised when there are no nodes available. A node is available when it's websocket is connected and not closed.
     """
-    pass
 
 
 class PlayerAlreadyExists(SlateException):
     """
     Raised when a player for the given channel's guild already exists.
     """
-    pass
 
 
 class TrackLoadFailed(SlateException):
@@ -102,8 +94,8 @@ class TrackLoadFailed(SlateException):
     def severity(self) -> str:
         """
         :py:class:`str`:
-            The severity of the error. See this `file <https://github.com/sedmelluq/lavaplayer/blob/01dfac5fea1bf683d2a9bc8c8c28589099eb2540/main/src/main/java/com/sedmelluq/discord/lavaplayer/tools/FriendlyException.java#L26-L43>`_
-            for more information.
+            The severity of the error. See this `file <https://github.com/sedmelluq/lavaplayer/blob/01dfac5fea1bf683d2a9bc8c8c28589099eb2540/main/src/main/java/com/sedmelluq
+            /discord/lavaplayer/tools/FriendlyException.java#L26-L43>`_ for more information.
         """
         return self._severity
 
