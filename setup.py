2946bb5b0c9ce594ec1d384e3d0780a4336c0f48
Aaron Hennessey <axelancerr@gmail.com>
2021-06-17 04:38:37 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,33 +10,33 @@ with open('README.md', 'r') as file:
 with open('requirements.txt') as file:
     INSTALL_REQUIRES = file.read().splitlines()
 
-with open(os.path.abspath(os.path.join(os.path.dirname(__file__), 'slate/__init__.py'))) as f:
-    VERSION = re.search(r'^__version__\s*(:[^=]*)?=\s*[\'"]([^\'"]*)[\'"]', f.read(), re.MULTILINE).group(2)
+with open(os.path.abspath(os.path.join(os.path.dirname(__file__), 'slate/__init__.py'))) as file:
+    VERSION = re.search(r'^__version__: [^=]* = f\'([^\']*)\'', file.read(), re.MULTILINE).group(1)
 
 
 CLASSIFIERS = [
-    'Development Status :: 4 - Beta',
     'Framework :: AsyncIO',
-    'Intended Audience :: Developers',
-    'License :: OSI Approved :: MIT License',
-    'Natural Language :: English',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3 :: Only',
-    'Programming Language :: Python :: 3.8',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
-    'Programming Language :: Python :: Implementation :: CPython',
     'Topic :: Software Development',
     'Topic :: Software Development :: Libraries',
     'Topic :: Software Development :: Libraries :: Python Modules',
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3 :: Only',
+    'Programming Language :: Python :: 3.9',
+    'Programming Language :: Python :: 3.10',
+    'Programming Language :: Python :: Implementation :: CPython',
+    'Operating System :: OS Independent',
+    'Environment :: Other Environment',
+    'Intended Audience :: Developers',
+    'Natural Language :: English',
     'Typing :: Typed'
 ]
 
 PROJECT_URLS = {
     'Documentation': 'https://slate-py.readthedocs.io/en/latest/',
-    'Source': 'https://github.com/Axelancerr/Slate',
     'Issue Tracker': 'https://github.com/Axelancerr/Slate/issues',
+    'Source': 'https://github.com/Axelancerr/Slate',
 }
 
 
@@ -51,14 +51,14 @@ EXTRAS_REQUIRE = {
 setuptools.setup(
     author='Axelancerr',
     classifiers=CLASSIFIERS,
-    description='A Lavalink and Andesite wrapper.',
+    description='A Lavalink, Andesite and Obsidian websocket/api wrapper.',
     extras_require=EXTRAS_REQUIRE,
     install_requires=INSTALL_REQUIRES,
     license='MIT',
     name='Slate',
-    packages=['slate', 'slate.andesite', 'slate.lavalink', 'slate.bases', 'slate.objects'],
+    packages=['slate', 'slate.andesite', 'slate.lavalink', 'slate.objects', 'slate.obsidian'],
     project_urls=PROJECT_URLS,
-    python_requires=">=3.8",
+    python_requires=">=3.9",
     url='https://github.com/Axelancerr/Slate',
     version=VERSION,
     long_description=LONG_DESCRIPTION,
