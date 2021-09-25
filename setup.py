5fd975abf7a8748a27d52a6b9718d29da74d3bc9
Aaron Hennessey <axelancerr@gmail.com>
2021-09-25 16:33:56 +0100

    update base files

--- setup.py
+++ setup.py
@@ -1,65 +1,67 @@
-import os
+# Future
+from __future__ import annotations
+
+# Standard Library
 import re
 
+# Packages
 import setuptools
 
 
-with open('README.md', 'r') as file:
+with open("README.md", "r") as file:
     LONG_DESCRIPTION = file.read()
 
-with open('requirements.txt') as file:
+with open("requirements.txt") as file:
     INSTALL_REQUIRES = file.read().splitlines()
 
-with open(os.path.abspath(os.path.join(os.path.dirname(__file__), 'slate/__init__.py'))) as file:
+with open("slate/__init__.py") as file:
     VERSION = re.search(r"^__version__: [^=]* = \"([^\"]*)\"", file.read(), re.MULTILINE).group(1)
 
-
 CLASSIFIERS = [
-    'Framework :: AsyncIO',
-    'Topic :: Software Development',
-    'Topic :: Software Development :: Libraries',
-    'Topic :: Software Development :: Libraries :: Python Modules',
-    'Development Status :: 4 - Beta',
-    'License :: OSI Approved :: MIT License',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3 :: Only',
-    'Programming Language :: Python :: 3.9',
-    'Programming Language :: Python :: 3.10',
-    'Programming Language :: Python :: Implementation :: CPython',
-    'Operating System :: OS Independent',
-    'Environment :: Other Environment',
-    'Intended Audience :: Developers',
-    'Natural Language :: English',
-    'Typing :: Typed'
+    "Framework :: AsyncIO",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+    "Development Status :: 4 - Beta",
+    "License :: OSI Approved :: MIT License",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Operating System :: OS Independent",
+    "Environment :: Other Environment",
+    "Intended Audience :: Developers",
+    "Natural Language :: English",
+    "Typing :: Typed"
 ]
 
 PROJECT_URLS = {
-    'Documentation': 'https://slate-py.readthedocs.io/en/latest/',
-    'Issue Tracker': 'https://github.com/Axelancerr/Slate/issues',
-    'Source': 'https://github.com/Axelancerr/Slate',
+    "Documentation": "https://slate.readthedocs.io/en/latest/",
+    "Issue Tracker": "https://github.com/Axelancerr/slate/issues",
+    "Source":        "https://github.com/Axelancerr/slate",
 }
 
-
 EXTRAS_REQUIRE = {
-    'docs': [
-        'sphinx',
-        'sphinxcontrib_trio',
-        'faculty-sphinx-theme',
+    "docs": [
+        "sphinx",
+        "sphinxcontrib_trio",
+        "faculty-sphinx-theme",
     ],
 }
 
 setuptools.setup(
-    author='Axelancerr',
+    author="Axelancerr",
     classifiers=CLASSIFIERS,
-    description='A Lavalink, Andesite and Obsidian websocket/api wrapper.',
+    description='A lavalink, andesite and obsidian websocket/api wrapper.',
     extras_require=EXTRAS_REQUIRE,
     install_requires=INSTALL_REQUIRES,
-    license='MIT',
-    name='Slate',
-    packages=['slate', 'slate.andesite', 'slate.lavalink', 'slate.objects', 'slate.obsidian', 'slate.utils', 'slate.obsidian.objects'],
+    include_package_data=True,
+    license="MIT",
+    name="slate",
+    packages=["slate", "slate.andesite", "slate.lavalink", "slate.obsidian", "slate.objects", "slate.utils", "slate.obsidian.objects"],
     project_urls=PROJECT_URLS,
-    python_requires=">=3.9",
-    url='https://github.com/Axelancerr/Slate',
+    python_requires=">=3.9.0",
+    url="https://github.com/Axelancerr/slate",
     version=VERSION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
