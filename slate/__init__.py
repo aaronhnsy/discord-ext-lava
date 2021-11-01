10f79486915f257c14ec8fe9294a8b890289ec8f
Axel <axelancerr@gmail.com>
2021-11-01 21:52:08 +0000

    version bump to v2021.11.01

--- slate/__init__.py
+++ slate/__init__.py
@@ -22,13 +22,13 @@ class VersionInfo(NamedTuple):
     serial: int
 
 
-version_info: Final[VersionInfo] = VersionInfo(major=2021, minor=10, micro=30, releaselevel="final", serial=0)
+version_info: Final[VersionInfo] = VersionInfo(major=2021, minor=11, micro=1, releaselevel="final", serial=0)
 
 __title__: Final[str] = "slate"
 __author__: Final[str] = "Axelancerr"
 __copyright__: Final[str] = "Copyright 2020-present Axelancerr"
 __license__: Final[str] = "MIT"
-__version__: Final[str] = "2021.10.30"
+__version__: Final[str] = "2021.11.01"
 __maintainer__: Final[str] = "Aaron Hennessey"
 __source__: Final[str] = "https://github.com/Axelancerr/slate"
 
